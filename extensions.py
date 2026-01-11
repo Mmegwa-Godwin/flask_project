a382b0ea9d2180aaebe49351817b00cdfcd207c2
charli-bash <charlesejike9111@gmail.com>
2026-01-11 15:52:35 -0800

    ex

--- extensions.py
+++ extensions.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from flask_wtf.csrf import CSRFProtect
+from flask_login import LoginManager # type: ignore
+from flask_wtf.csrf import CSRFProtect # type: ignore
 
 db = SQLAlchemy()
 login_manager = LoginManager()
